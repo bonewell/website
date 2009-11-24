e55fb3c24378e1afe367268f7178848400d4eb96
bone <bone@b384e094-d8f3-11de-8f42-071d230371f4>
2009-11-24 12:20:18 +0000

    suport vetki
    git-svn-id: file:///home/bone/Projects/svn/bone@1 b384e094-d8f3-11de-8f42-071d230371f4

---
+++ trunk/scripts/changestyle.js
@@ -0,0 +1,55 @@
+/**
+ * Скрип для смены стиля сайта
+ */
+function getCookie(name) {
+   var cookie = " " + document.cookie;
+   var search = " " + name + "=";
+   var setStr = null;
+   var offset = 0;
+   var end = 0;
+   if (cookie.length > 0) {
+      offset = cookie.indexOf(search);
+      if (offset != -1) {
+         offset += search.length;
+         end = cookie.indexOf(";", offset)
+         if (end == -1) {
+            end = cookie.length;
+         }
+         setStr = unescape(cookie.substring(offset, end));
+      }
+   }
+   return(setStr);
+}
+function change(sid)
+{
+   var obj = document.getElementById("style");
+   if (sid==3)
+   {
+      obj.href="styles_home.css";
+   }
+   else if (sid==2)
+   {
+      obj.href="styles.css";
+   }
+   else
+   {
+      obj.href="styles_wood.css";
+   }
+}
+style_id=1;
+//читаем значение стиля из куки
+style_id = getCookie("sid");
+change(style_id);
+function loadstyle()
+{
+   style_id = getCookie("sid");
+   var obj = document.getElementById("mystyle");
+   obj.value=style_id;
+}
+function changestyle()
+{
+   var obj = document.getElementById("mystyle");
+   style_id=obj.value;
+   document.cookie="sid="+style_id+"; path=/; expires=Fri, 31-Dec-2032 00:00:00 GMT";
+   change(style_id);
+}
