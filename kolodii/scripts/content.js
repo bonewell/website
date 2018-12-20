ff5e692fea9065a0d994ed0ea1fcabf7b6f462c6
Konstantin Kolodiy <bonewell@yandex.ru>
2018-12-20 21:52:17 +0800

    Do refactoring

---
+++ kolodii/scripts/content.js
@@ -0,0 +1,27 @@
+function load() {
+    if (!ssi) {
+        loadblock('head');
+        loadblock('menu');
+        loadblock('foot');
+        loadpage();
+    }
+    loadstyle();
+}
+
+function loadblock(name) {
+    loadhtml(name, '#' + name);
+}
+
+function loadhtml(page, id) {
+    var url = 'html/' + page + '.html';
+    $.ajax(url).done(function(data) {
+        $(id).html(data);
+    }).fail(function() {
+        alert('Could not load');
+    });
+    return false;
+}
+
+function show(page) {
+    return loadhtml(page, '#main');
+}
